0ffe7b7be42b5466e7837d7c8cd4d13da0fd82f3
Deeptanu Bhowmik <117512172+Deeptanu20024@users.noreply.github.com>
2024-06-19 09:19:21 +0000

    new changes

---
+++ typescript_basics/src/function.js
@@ -0,0 +1,9 @@
+"use strict";
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(value) {
+    return value.toUpperCase();
+}
+console.log(getUpper("hello"));
+console.log(addTwo(5));
--- typescript_basics/src/function.ts
+++ typescript_basics/src/function.ts
@@ -1,4 +1,24 @@
-function addTwo(num: number){
+function addTwo(num: number): number{
+    // return "hello";
     return num+2;
 }
-console.log(addTwo(5))
+
+
+function getUpper(value: string){
+    return value.toUpperCase()
+}
+
+
+
+function getValue(myVal:number){
+    if(myVal>5){
+        return true;
+    }
+    return "ok 200"
+}
+
+// console.log(getUpper("hello"))
+
+// console.log(addTwo(5))
+
+console.log()
